c09cb75d377ccf03429b76bc75c38b4606e5ac6c
andre <andrea.markvukaj@mail.polimi.it>
2026-04-19 13:59:53 +0200

    - Removed getType method. - Moved applyTo logic to every CharacterCard's subclass. - Added JavaDoc for listed changes.

--- Mesos/src/main/java/it/polimi/ingsw/model/card/ArtistCard.java
+++ Mesos/src/main/java/it/polimi/ingsw/model/card/ArtistCard.java
@@ -1,5 +1,8 @@
 package it.polimi.ingsw.model.card;
+import it.polimi.ingsw.model.card.building.BuildingCard;
 import it.polimi.ingsw.model.game.Era;
+import it.polimi.ingsw.model.player.Player;
+import it.polimi.ingsw.model.player.Tribe;
 
 /**
  * Class representing Artist Card.
@@ -13,8 +16,25 @@ public class ArtistCard extends CharacterCard{
         super(era, id);
     }
 
+    /**
+     * Applies this ARTIST card to the given player.
+     * Behavior:
+     * Adding the character to the player's tribe.
+     * Granting food based on {@link #getFoodOnAcquired(Tribe)}
+     * Triggering building effects that react to character acquisition.
+     *
+     * @param player the player acquiring the card
+     */
     @Override
-    public CharacterType getType() {
-        return CharacterType.ARTIST;
+    public void applyTo(Player player) {
+        Tribe tribe = player.getTribe();
+
+        tribe.getMembers().get(CharacterType.ARTIST).add(this);
+
+        player.addFood(getFoodOnAcquired(tribe));
+
+        for (BuildingCard b : tribe.getBuildings()) {
+            b.onCharacterAdded(this, player);
+        }
     }
 }
--- Mesos/src/main/java/it/polimi/ingsw/model/card/BuilderCard.java
+++ Mesos/src/main/java/it/polimi/ingsw/model/card/BuilderCard.java
@@ -1,5 +1,8 @@
 package it.polimi.ingsw.model.card;
+import it.polimi.ingsw.model.card.building.BuildingCard;
 import it.polimi.ingsw.model.game.Era;
+import it.polimi.ingsw.model.player.Player;
+import it.polimi.ingsw.model.player.Tribe;
 
 /**
  * Class representing Builder Card.
@@ -37,8 +40,25 @@ public class BuilderCard extends CharacterCard{
         return builderPrestige;
     }
 
+    /**
+     * Applies this BUILDER card to the given player.
+     * Behavior:
+     * Adding the character to the player's tribe.
+     * Granting food based on {@link #getFoodOnAcquired(Tribe)}
+     * Triggering building effects that react to character acquisition.
+     *
+     * @param player the player acquiring the card
+     */
     @Override
-    public CharacterType getType() {
-        return CharacterType.BUILDER;
+    public void applyTo(Player player) {
+        Tribe tribe = player.getTribe();
+
+        tribe.getMembers().get(CharacterType.BUILDER).add(this);
+
+        player.addFood(getFoodOnAcquired(tribe));
+
+        for (BuildingCard b : tribe.getBuildings()) {
+            b.onCharacterAdded(this, player);
+        }
     }
 }
