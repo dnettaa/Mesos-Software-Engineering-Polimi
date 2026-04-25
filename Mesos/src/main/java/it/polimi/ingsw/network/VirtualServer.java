464618aa78b897e15e13d9552e581d89c0f8df47
dnettaa <dianaagata.francesconi@mail.polimi.it>
2026-04-25 12:33:42 +0200

    Add virtual server network interface

---
+++ Mesos/src/main/java/it/polimi/ingsw/network/VirtualServer.java
@@ -0,0 +1,38 @@
+//contratto lato client
+
+package it.polimi.ingsw.network;
+
+import it.polimi.ingsw.message.ClientMessage;
+import it.polimi.ingsw.model.player.TotemColor;
+
+public interface VirtualServer {
+
+    /**
+     * Creates a new lobby and joins it as the first player.
+     *
+     * @param nickname the nickname chosen by the player
+     * @param color the totem color chosen by the player
+     * @param expectedPlayers the number of players required to start the game
+     */
+    void createLobby(String nickname, TotemColor color, int expectedPlayers);
+
+    /**
+     * Joins an existing lobby.
+     *
+     * @param nickname the nickname chosen by the player
+     * @param color the totem color chosen by the player
+     */
+    void joinLobby(String nickname, TotemColor color);
+
+    /**
+     * Sends a generic client message to the server.
+     *
+     * @param message the message representing a client-side action
+     */
+    void sendMessage(ClientMessage message);
+
+    /**
+     * Closes the connection with the server.
+     */
+    void disconnect();
+}
