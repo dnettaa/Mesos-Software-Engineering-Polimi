022a812d2c43327bbdb81118764e394b9e78ee35
dnettaa <dianaagata.francesconi@mail.polimi.it>
2026-04-28 09:23:15 +0200

    Add base network message class

---
+++ Mesos/src/main/java/it/polimi/ingsw/network/message/Message.java
@@ -0,0 +1,16 @@
+package it.polimi.ingsw.network.message;
+
+import java.io.Serializable;
+
+/**
+ * Base class for all messages exchanged between client and server.
+ * <p>
+ * Messages are serializable because they must be transferable through both
+ * Socket object streams and RMI method calls.
+ */
+
+public abstract class Message implements Serializable{
+    private static final long serialVersionUID = 1L;
+
+
+}
--- Mesos/src/main/java/it/polimi/ingsw/network/message/ServerMessage.java
+++ Mesos/src/main/java/it/polimi/ingsw/network/message/ServerMessage.java
@@ -10,7 +10,7 @@ import it.polimi.ingsw.view.View;
  * statements or instanceof checks.
  */
 
-public abstract class ServerMessage extends Message{
+public abstract class ServerMessage {
 
     /**
      * Applies this message to the given client-side view.
