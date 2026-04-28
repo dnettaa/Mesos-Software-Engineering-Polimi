d79723e9ed852ccec303502607b103918c6d89b8
dnettaa <dianaagata.francesconi@mail.polimi.it>
2026-04-28 10:04:15 +0200

    Add place totem client message

---
+++ Mesos/src/main/java/it/polimi/ingsw/network/message/PlaceTotemMessage.java
@@ -0,0 +1,41 @@
+package it.polimi.ingsw.network.message;
+
+import it.polimi.ingsw.network.VirtualView;
+import it.polimi.ingsw.controller.GameController;
+
+/**
+ * Message sent by a client to place their totem on an offer slot.
+ */
+
+public class PlaceTotemMessage extends ClientMessage{
+    private final char slotID;
+
+    /**
+     * Creates a new place-totem message.
+     *
+     * @param slotID the identifier of the offer slot where the player wants to place the totem
+     */
+    public PlaceTotemMessage(char slotID) {
+        this.slotID = slotID;
+    }
+
+    /**
+     * Returns the selected offer slot ID.
+     *
+     * @return the slot identifier
+     */
+
+    public char getSlotID(){
+        return slotID;
+    }
+    /**
+     * Executes the place-totem action on the controller.
+     *
+     * @param controller the game controller handling the request
+     * @param sender the virtual view associated with the client who sent the message
+     */
+    @Override
+    public void execute(GameController controller, VirtualView sender){
+        controller.placeTotem(sender, slotID);
+    }
+}
