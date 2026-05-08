3d9dc21b5ae27bf2e28d8041f83ff70e424b2cb8
andre <andrea.markvukaj@mail.polimi.it>
2026-05-08 03:36:26 +0200

    - Implemented MatchResultRepository.java interface class.

--- Mesos/src/main/java/it/polimi/ingsw/leaderboard/MatchResultRepository.java
+++ Mesos/src/main/java/it/polimi/ingsw/leaderboard/MatchResultRepository.java
@@ -1,4 +1,10 @@
 package it.polimi.ingsw.leaderboard;
 
+import java.util.List;
+
 public interface MatchResultRepository {
+
+    void save(MatchResult result);
+
+    List<MatchResult> findByPlayerCount(int playerCount);
 }
