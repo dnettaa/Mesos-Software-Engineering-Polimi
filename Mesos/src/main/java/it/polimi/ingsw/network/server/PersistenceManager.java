5f7207e93821c62f503ce25abf0d313e500e5ff8
Vadym <vadym.kitsul@mail.polimi.it>
2026-05-11 00:06:16 +0200

    Fixed some comments

--- Mesos/src/main/java/it/polimi/ingsw/network/server/PersistenceManager.java
+++ Mesos/src/main/java/it/polimi/ingsw/network/server/PersistenceManager.java
@@ -17,11 +17,8 @@ public class PersistenceManager {
 
     private static final String SAVE_FILE = "mesos_persistence.json";
 
-    // --- IL NUOVO GSON BUILDER "INTELLIGENTE" ---
     private static final Gson gson = new GsonBuilder()
-            // Diciamo a GSON di usare l'Adapter per le Carte (incluse tutte le sue sottoclassi astratte)
             .registerTypeHierarchyAdapter(Card.class, new InterfaceAdapter<>())
-            // Diciamo a GSON di usare l'Adapter anche per le Fasi del gioco (InGamePhase, EndRoundPhase, ecc.)
             .registerTypeHierarchyAdapter(Phase.class, new InterfaceAdapter<>())
             .setPrettyPrinting()
             .create();
@@ -31,7 +28,6 @@ public class PersistenceManager {
      */
     public static void saveGame(GameActions game) {
         try (Writer writer = new FileWriter(SAVE_FILE)) {
-            // GSON salverà i campi della classe concreta Game passata come parametro
             gson.toJson(game, writer);
             System.out.println("[PERSISTENCE] Stato della partita salvato con successo.");
         } catch (IOException e) {
@@ -48,7 +44,6 @@ public class PersistenceManager {
         if (!file.exists()) return null;
 
         try (Reader reader = new FileReader(file)) {
-            // Specifichiamo la classe concreta Game.class per la deserializzazione
             GameActions loadedGame = gson.fromJson(reader, Game.class);
             System.out.println("[PERSISTENCE] Partita ripristinata dal disco!");
             return loadedGame;
