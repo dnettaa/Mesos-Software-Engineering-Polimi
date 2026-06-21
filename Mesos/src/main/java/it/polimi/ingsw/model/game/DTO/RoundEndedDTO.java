a49b6b908f84916b2d566e7e9877dfc9baff115f
andre <andrea.markvukaj@mail.polimi.it>
2026-06-21 16:31:33 +0200

    - Added JavaDoc to DTO package classes

--- Mesos/src/main/java/it/polimi/ingsw/model/game/DTO/RoundEndedDTO.java
+++ Mesos/src/main/java/it/polimi/ingsw/model/game/DTO/RoundEndedDTO.java
@@ -5,6 +5,25 @@ import it.polimi.ingsw.model.game.Era;
 import java.util.List;
 import java.io.Serializable;
 
+/**
+ * DTO broadcast after the board has been refreshed for a new round.
+ *
+ * @param discardedLowerTribeIDs identifiers of tribe cards discarded from the lower row
+ * @param discardedLowerEventIDs identifiers of event cards discarded from the lower row
+ * @param movedUpperToLowerTribeIDs identifiers of tribe cards moved from upper to lower row
+ * @param discardedLowerBuildingIDs identifiers of building cards discarded from the lower row
+ * @param movedUpperToLowerBuildingIDs identifiers of building cards moved from upper to lower row
+ * @param newUpperRowIDs identifiers of cards newly placed in the upper row
+ * @param newLowerRowIDs identifiers of cards newly placed in the lower row
+ * @param revealedBuildingIDs identifiers of newly revealed building cards
+ * @param newEra the era active after the round transition
+ * @param newRound the round number reached after the transition
+ * @param newTurnOrder player nicknames in the new turn order
+ * @param firstPlayerNickname the nickname of the first player of the new round
+ * @param tribeDeckRemaining the number of cards left in the tribe deck
+ *
+ * @author Luca Grecchi
+ */
 public record RoundEndedDTO(
         List<String> discardedLowerTribeIDs,
         List<String> discardedLowerEventIDs,
